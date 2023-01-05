56d44a9beef0bb340160cbf77e237895805de9c5
Dilan Nair <dilan@dilanxd.com>
2023-01-05 09:31:30 -0600

    Update Footer.svelte

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,16 +1,7 @@
-<script lang="ts">
-  import watermark from './assets/website-watermark.svg'
-</script>
-
 <footer>
   <p>
     Copyright © Overture Games, LLC. All rights reserved.
   </p>
-  <div>
-    <a href="https://www.dilanxd.com" target="_blank" rel="noreferrer">
-      <img src={watermark} alt="website created by Dilan Nair" />
-    </a>
-  </div>
 </footer>
 
 <style lang="scss">
@@ -18,29 +9,6 @@
     background-color: #1d1731;
     margin: 0;
     padding: 32px;
-  }
-
-  div {
-    display: flex;
-    justify-content: center;
-    margin-top: 16px;
-   
-    a {
-      opacity: 0.5;
-      transition: opacity 150ms;
-      
-      &:hover {
-        opacity: 1;
-      }
-    }
-
-    img {
-      width: 280px;
-
-      @media (min-width: $desktop) {
-        width: 320px;
-      }
-    }
   }
 
   p {
